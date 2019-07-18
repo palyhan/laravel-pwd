091480d5a90b38b84f93b582145447f7d72b3ca5
kidkang <723616880@qq.com>
2019-07-18 16:12:37 +0800

    1.0.0

--- src/PwdValidator.php
+++ src/PwdValidator.php
@@ -10,28 +10,15 @@ class PwdValidator extends Validator
     {
         parent::__construct($translator, $data, $rules, $messages);
     }
-    // public function validate($attribute, $value, $parameters, $validator){
-    //     //$validator->getValue();
-    //     dd($validator->getValue());
-    //     //dd(123);
-    //     return false;
-    // }
-    // public function ValidateTags($attribute,$value,$parameters){
-
-    // }
 
     public function validatePwd($attribute, $value, $parameters, $validator)
     {
         list($table, $user) = $parameters;
         $compireField       = isset($parameters[2]) && $parameters[2] ? $parameters[2] : 'password';
-        //dd($user);
-        //dd($this->getValue('account'));
-        $re = DB::table($table)
+        $re                 = DB::table($table)
             ->select(DB::raw("md5(concat('$value',salt)) as $attribute ,$compireField"))
             ->where($user, $this->getValue($user))
             ->first();
-        //dd($re);
-        //->toArray();
         if (!$re) {
             return false;
         }
