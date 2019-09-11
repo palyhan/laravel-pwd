0fef89d3c9cd2f2176a794ebfe3382ac8250fc8f
kidkang <723616880@qq.com>
2019-09-11 15:24:22 +0800

    modify validation function

--- src/PwdValidator.php
+++ src/PwdValidator.php
@@ -22,16 +22,20 @@ class PwdValidator extends Validator
             $db = DB::table($table);
         }
         $compireField       = isset($parameters[2]) && $parameters[2] ? $parameters[2] : 'password';
+
+        $userField = $user;
+        $userValue = $user;
+        if(strpos($user, '.')){
+            list($userField,$userValue) = explode('.', $user);
+        }
         $re                 = $db
             ->select(DB::raw("md5(concat('$value',salt)) as $attribute ,$compireField"))
-            ->where($user, $this->getValue($user))
+            ->where($userField, $this->getValue($userValue))
             ->first();
         if (!$re) {
             return false;
         }
-
         $re = collect($re);
-
         $attributeValue = $re->get($attribute);
         $compireValue   = $re->get($compireField);
         return $attributeValue === $compireValue;
